6143b51e409f50ed16d596851d7a988252a9c94d
Woodensponge <43834352+Woodensponge@users.noreply.github.com>
2022-12-05 22:15:45 -0500

    Uhhuh

---
+++ uk-mod-rumble/RumbleMod.cs
@@ -0,0 +1,94 @@
+﻿using UMM;
+using BepInEx;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//https://twitter.com/ShammyTV/status/1599929049794519041
+
+namespace RumbleMod
+{
+#if UMM
+    [UKPlugin("Rumble", "1.0.0", "Adds controller (or not a controller) rumble into the game.", true, true)]
+    public class RumbleMod : UKMod
+    {
+        public enum RumblePriority
+        {
+            None,
+            Screenshake,
+            Whiplash
+        }
+
+        public static RumbleMod Instance;
+        float currentRumble = 0f;
+        RumblePriority currentRumblePriority;
+
+        public float CurrentRumble { get => currentRumble; }
+        public RumblePriority CurrentRumblePriority { get => currentRumblePriority; set => currentRumblePriority = value; }
+
+        public override void OnModLoaded()
+        {
+            if (Instance != null)
+            {
+                DestroyImmediate(gameObject);
+                return;
+            }
+            Instance = this;
+
+            ScreenShakePatch.ApplyHooks();
+        }
+
+        public void SetRumble(float rumble)
+        {
+            Gamepad.current.SetMotorSpeeds(rumble, rumble);
+        }
+
+        public override void OnModUnload()
+        {
+            ScreenShakePatch.RemoveHooks();
+        }
+    }
+#endif
+
+#if BEPINEX
+    [BepInPlugin("woodensponge.ultrakill.rumble", "Rumble Mod", "1.0.0")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class RumbleMod : BaseUnityPlugin
+    {
+        public enum RumblePriority
+        {
+            None,
+            Screenshake,
+            Whiplash
+        }
+
+        public static RumbleMod Instance;
+        float currentRumble = 0f;
+        RumblePriority currentRumblePriority;
+
+        public float CurrentRumble { get => currentRumble; }
+        public RumblePriority CurrentRumblePriority { get => currentRumblePriority; set => currentRumblePriority = value; }
+
+        public void Start()
+        {
+            if (Instance != null)
+            {
+                DestroyImmediate(gameObject);
+                return;
+            }
+            Instance = this;
+
+            ScreenShakePatch.ApplyHooks();
+        }
+
+        public void SetRumble(float rumble)
+        {
+            Gamepad.current.SetMotorSpeeds(rumble, rumble);
+        }
+
+        public void OnDestroy()
+        {
+            ScreenShakePatch.RemoveHooks();
+        }
+    }
+#endif
+}
---
+++ uk-mod-rumble/ScreenShakePatch.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace RumbleMod
+{
+    public static class ScreenShakePatch
+    {
+        public static void ApplyHooks()
+        {
+            On.CameraController.Update += CameraController_Update;
+            On.HookArm.Update += HookArm_Update;
+        }
+
+        private static void HookArm_Update(On.HookArm.orig_Update orig, HookArm self)
+        {
+            orig(self);
+            if (RumbleMod.Instance.CurrentRumblePriority > RumbleMod.RumblePriority.Whiplash)
+            {
+                return;
+            }
+            if (self.state == HookState.Pulling)
+            {
+                RumbleMod.Instance.CurrentRumblePriority = RumbleMod.RumblePriority.Whiplash;
+                RumbleMod.Instance.SetRumble(1f);
+            }
+            else
+            {
+                if (RumbleMod.Instance.CurrentRumblePriority == RumbleMod.RumblePriority.Whiplash)
+                {
+                    RumbleMod.Instance.CurrentRumblePriority = RumbleMod.RumblePriority.None;
+                    RumbleMod.Instance.SetRumble(0f);
+                }
+            }
+        }
+
+        private static void CameraController_Update(On.CameraController.orig_Update orig, CameraController self)
+        {
+            orig(self);
+            if (RumbleMod.Instance.CurrentRumblePriority == RumbleMod.RumblePriority.Screenshake)
+            {
+                RumbleMod.Instance.CurrentRumblePriority = RumbleMod.RumblePriority.None;
+            }
+            if (RumbleMod.Instance.CurrentRumblePriority > RumbleMod.RumblePriority.Screenshake)
+            {
+                return;
+            }
+            RumbleMod.Instance.CurrentRumblePriority = RumbleMod.RumblePriority.Screenshake;
+            RumbleMod.Instance.SetRumble(Mathf.Clamp(self.cameraShaking / 2.0f, 0f, 1f));
+        }
+
+        public static void RemoveHooks()
+        {
+            On.CameraController.Update -= CameraController_Update;
+        }
+    }
+}
